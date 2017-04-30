caac8018b68d6322cc4a3f543f6d3fefbe8b41c7
10346010 <10346010@ntub.edu.tw>
2017-04-30 12:12:35 -0400

    new, edit and delete

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,49 @@
+class CandidatesController < ApplicationController
+  
+  before_action :find_candidate, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @candidates = Candidate.all
+  end
+  
+  def new
+    @candidate = Candidate.new
+  end
+  
+  def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to candidates_path, notice: "added!"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+
+  end
+  
+  def update 
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "updated!"
+    else
+      render 'edit'
+    end    
+  end
+  
+  def destroy
+    @candidate.destroy
+    redirect_to candidates_path, notice: "deleted!"
+  end
+  
+  private
+  def candidate_params
+    params.require("candidate").permit(:name, :party, :age, :politics)
+  end
+  
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
+    redirect_to candidates_path, notice: "no data!" if @candidate.nil?
+  end
+end
